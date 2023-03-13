a02b47b752138d864931720430832dcfe88fa09f
DenilDenilson <jordanpreseleccion@gmail.com>
2023-03-13 14:28:34 -0500

    Done CRUD

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 # create the extension
@@ -20,9 +20,26 @@ class Task(db.Model):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks = tasks) # la variable tasks puede llamarse como gustes
 
 @app.route("/create-task", methods=['POST'])
 def create():
     task = Task(content=request.form['content'], done = False)
-    #db.session.add() # Para usar esto tenemos que tener la tabla creada
+    db.session.add(task) # Para usar esto tenemos que tener la tabla creada
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route("/done/<id>")
+def done(id):
+    task = Task.query.filter_by(id=int(id)).first()
+    task.done = not(task.done)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/delete/<id>")
+def delete(id):
+    Task.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('home'))
+
